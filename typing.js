c510a5bc3427830a25671ed9fbec99efff491e8f
Aaquib <mohd.aaquib001@gmail.com>
2025-03-04 03:05:26 +0530

    all files added

---
+++ typing.js
@@ -0,0 +1,87 @@
+const typingText=document.querySelector('.typing-text p');
+const input=document.querySelector('.input-field');
+const time=document.querySelector('.time span b');
+const mistakes=document.querySelector('.mistakes span');
+const wpm=document.querySelector('.wpm span');
+const cpm=document.querySelector('.cpm span');
+const btn=document.querySelector('.try-again');
+
+let timer;
+let maxTime=60;
+let timeLeft=maxTime;
+let charIndex=0;
+let mistake=0;
+let istyping=false;
+
+function loadparagraph(){
+    const paragraph=[" Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"]
+    const randomIndex=Math.floor(Math.random()*paragraph.length);
+    typingText.innerHTML='';
+    for(const char of paragraph[randomIndex]){
+        typingText.innerHTML+=`<span>${char}</span>`;  
+    }
+    typingText.querySelectorAll('span')[0].classList.add('active')
+    document.addEventListener('keydown',()=>input.focus())
+    typingText.addEventListener("click",()=>{
+        input.focus()
+    })
+}
+loadparagraph();
+
+input.addEventListener("input",initTyping);
+
+function initTyping(){
+    const char=typingText.querySelectorAll('span');
+    const typeChar=input.value.charAt(charIndex);
+    
+    if(charIndex<char.length && timeLeft>0){
+
+        if(!istyping){
+            timer=setInterval(initTime,1000);
+            istyping=true;
+        }
+
+        if(char[charIndex].innerText===typeChar){
+            char[charIndex].classList.add('correct')
+        }
+        else{
+            mistake++;
+            char[charIndex].classList.add('incorrect')
+        }
+        charIndex++;
+        char[charIndex].classList.add('active')
+        mistakes.innerHTML= mistake;
+        cpm.innerHTML=charIndex-mistake;
+    }
+    else{
+        clearInterval(timer)
+        input.value='';
+    }
+}
+
+function initTime(){
+    if(timeLeft>0){
+        timeLeft--;
+        time.innerHTML=timeLeft;
+        let wpmval=Math.round(((charIndex-mistake)/5)/(maxTime-timeLeft)*60);
+        wpm.innerText=wpmval;
+    }
+    else{
+        clearInterval(timer);
+    }
+}
+
+btn.addEventListener('click',reset);
+
+function reset(){
+    loadparagraph();
+    clearInterval(timer);
+    timeLeft=maxTime;
+    time.innerHTML=timeLeft;
+    charIndex=0;
+    mistake=0;
+    istyping=false;
+    wpm.innerHTML=0;
+    cpm.innerHTML=0;
+    mistakes.innerHTML=0;
+}
